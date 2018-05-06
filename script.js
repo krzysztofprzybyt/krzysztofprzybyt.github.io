6d70f4c414e81604e10efed756afb523892fa6ec
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2018-05-06 22:36:16 +0200

    upgrade

--- script.js
+++ script.js
@@ -139,11 +139,8 @@ $(document).ready(function() {
         title: taskTitle,
         content: taskContent
       }),
-      complete: function(data) {
-        if(data.status === 200) {
-          getAllTasks();
-        }
-      }
+      
+      success: getAllTasks
     });
   }
 
