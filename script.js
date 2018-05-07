90531a22370c16d2f540d5fcc67313d7d4ddf7b7
krzysztofprzybyt <krzysztof.przybyt@gmail.com>
2018-05-07 23:44:56 +0200

    upgrade08

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   const apiRoot = 'https://warm-cove-95326.herokuapp.com/v1/';
   const trelloApiRoot = 'https://warm-cove-95326.herokuapp.com/v1/trello/';
-    const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
+  const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
   var availableBoards = {};
